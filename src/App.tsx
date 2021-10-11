6db780e41a070915aa0083fd55ef4d128a3ec21b
Alexis Fournier <pro.fournieralexis@gmail.com>
2021-10-11 10:04:30 +0200

    Create main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./styles/global.css";
+import { Header } from "./components/module/Header/Header";
+import { Exception } from "./components/views/Exception/Exception";
+import { Home } from "./components/views/Home/Home";
+import { Footer } from "./components/module/Footer/Footer";
+import { Location } from "./components/views/Location/Location";
+import React from "react";
+import { DaoLocation } from "./types/DaoLocation";
+import { Dbal } from "./model/Dbal";
+import { About } from "./components/views/About/About";
+import { ErrorBoundary } from "./ErrorBoundary";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type State = {
+  locations: DaoLocation[];
+  locationSelected?: DaoLocation;
+};
+export default class App extends React.Component<{}, State> {
+  state: State = {
+    locations: [],
+    locationSelected: undefined,
+  };
+
+  componentDidMount() {
+    new Dbal()
+      .getLocations()
+      .then((loc) => {
+        this.setState({ locations: loc });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    return (
+      <ErrorBoundary>
+        <Router>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home locations={this.state.locations}></Home>
+            </Route>
+            <Route exact path="/location">
+              <Location locations={this.state.locations}></Location>
+            </Route>
+            <Route exact path="/about" component={About} />
+            <Route>
+              <Exception
+                code={404}
+                error={"Oups! La page que vous demandez n'éxiste pas."}
+              ></Exception>
+            </Route>
+          </Switch>
+          <Footer></Footer>
+        </Router>
+      </ErrorBoundary>
+    );
+  }
 }
-
-export default App;
